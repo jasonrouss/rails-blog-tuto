98b2862a31277a622d31c14ce0ba87ab58df1d89
jasonrouss <jasonrouss@gmail.com>
2023-05-04 16:39:31 +0300

    create our basic blog

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :blog_posts
+  
+  # get "/blog_posts/new",  to:"blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # get "/blog_posts/:id/edit",  to:"blog_posts#edit", as: :edit_blog_post
+  # patch "/blog_posts/:id",  to:"blog_posts#update"
+  # delete "/blog_posts/:id",  to:"blog_posts#destroy"
+
+  # afterwards because it won't match anwything with id afterwards
+
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+
+  # Defines the root path route ("/")
+   root "blog_posts#index"
+end
