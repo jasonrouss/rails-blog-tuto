36462d2413f1bfbccbd99de540bb8311ee251650
jasonrouss <jasonrouss@gmail.com>
2023-05-06 19:26:35 +0300

    final fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+#  #100.times do |i|
+    #blog_post = BlogPost.where(title: "Blog Post #{i}").first_or_initialize 
+    #blog_post.update(content: "Hello world", published_at: Time.current)
+ #end
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
@@ -8,7 +11,3 @@
 
 
 
- #100.times do |i|
-    #blog_post = BlogPost.where(title: "Blog Post #{i}").first_or_initialize 
-    #blog_post.update(content: "Hello world", published_at: Time.current)
- #end
